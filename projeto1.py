606161e9af425e775c7507014e726899268dcb48
Robert-Data-Analyst <ras.tech25@gmail.com>
2025-09-30 21:40:03 -0300

    Add files via upload
    subindo o arquivo

---
+++ projeto1.py
@@ -0,0 +1,8 @@
+# Projeto - Porto Fácil (Análise de Dados com Pandas)
+
+""""
+Importar dados fictícios de movimentação portuária (CSV)
+Limpar, filtrar, agrupar e gerar estatísticas
+Criar relatório consolidado com groupby, merge, sort_values
+"""
+
